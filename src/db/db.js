31c6fac7f4e569412d8ea35be4382f6b8ae5c6ad
Damaiceno Castro <cenocastro01@gmail.com>
2023-08-14 14:12:49 +0200

    creating a db to store user data

---
+++ src/db/db.js
@@ -0,0 +1,25 @@
+const DATA = [
+    {
+        "id":1,
+        "name": 'Person 1'
+    },
+    {
+        "id": 2,
+         "nome": 'Person 7'
+    },
+    {
+        "id":3, 
+        "nome": 'Person 3'
+    },
+    {
+        "id":4, 
+        "nome": 'Person 3'
+    },
+    {
+        "id": 5, 
+        "nome": 'Person 2'
+    }
+]
+
+
+export default DATA
