627aeaadfe787140e2c6e0ea9f915a562b983b24
Damaiceno Castro <cenocastro01@gmail.com>
2023-08-14 15:03:52 +0200

    updating ....

---
+++ src/pages/main_page1.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import Card_mensage from "../components/cards/card_mensage";
+
+export default function Main_page1(){
+    return(
+        <View>
+            <Card_mensage/>
+        </View>
+    )
+}
